d71fbacaf32447cf5dfa1d64c29ac66b080791b8
Simon Bourque <simon.bourque@live.ca>
2017-11-25 15:57:24 -0500

    Add notifications when pet status changes

--- app/src/main/java/plantfueled/puppysitter/PetNotification.java
+++ app/src/main/java/plantfueled/puppysitter/PetNotification.java
@@ -1,20 +1,28 @@
 package plantfueled.puppysitter;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class PetNotification {
+
+    private static final int NOTIFICATION_UNIQUE_ID = "PET_SITTER_NOTIFICATION_ID_plantfueled.puppysitter".hashCode();
 
     // Notification stuff
     private Context context;
     private int duration = Toast.LENGTH_SHORT;
     private Toast myToast;
 
+    private NotificationManager notificationManager;
+
     PetNotification(Context context){
         this.context = context;
         myToast = Toast.makeText(context, null, duration);
+
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     public void notHungry(String petName){
@@ -40,10 +48,12 @@ public class PetNotification {
                 text = petName + " is full and can't eat anymore";
                 break;
         }
-        if(text != "")
+        if(text != "") {
             toast(text);
+            notify(text);
+        }
 
-    };
+    }
 
     public void lonelyChange(Pet.LonelyStat stat, String petName){
         CharSequence text = "";
@@ -62,9 +72,11 @@ public class PetNotification {
                 text = petName + " socialized as much as it could";
                 break;
         }
-        if(text != "")
+        if(text != "") {
             toast(text);
-    };
+            notify(text);
+        }
+    }
 
     public void temperatureChange(Pet.TemperatureStat stat, String petName){
         CharSequence text = "";
@@ -80,9 +92,11 @@ public class PetNotification {
                 text = petName + " feels hot";
                 break;
         }
-        if(text != "")
+        if(text != "") {
             toast(text);
-    };
+            notify(text);
+        }
+    }
 
     public void hide(){
         toast("You need to move closer to your pet");
@@ -101,6 +115,11 @@ public class PetNotification {
                 myToast.show();
             }
         });
+    }
+
+    private void notify(CharSequence text) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context).setSmallIcon(R.drawable.puppy).setContentTitle("Puppy Alert").setContentText(text);
+        notificationManager.notify(NOTIFICATION_UNIQUE_ID, builder.build());
     }
 
 }
